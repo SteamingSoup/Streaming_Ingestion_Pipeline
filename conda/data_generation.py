55b8daafdd825553e067f952e547500bb39c4db7
Nick <67605183+SteamingSoup@users.noreply.github.com>
2021-09-27 22:17:23 -0500

    Update data_generation.py
    added comments

--- conda/data_generation.py
+++ conda/data_generation.py
@@ -1,21 +1,21 @@
-from faker import Faker
-import psycopg2
-from time import sleep
-from datetime import datetime
+from faker import Faker # package used to generate fake data
+import psycopg2 # database adapter for python so communicate with PostgreSQL database using python
+from time import sleep # suspends execution for so many seconds
+from datetime import datetime # for manipulating dates and times
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine # creates new engine object
 
-if __name__ == '__main__':
-    conn = create_engine("postgresql://TEST:password@localhost/TEST") 
-    faker = Faker() 
+if __name__ == '__main__': # if file is run directly
+    conn = create_engine("postgresql://TEST:password@localhost/TEST") # create engine
+    faker = Faker()       ## //user:password@hostname/database_name
     fields = ['job','company','residence','username','name','sex','address','mail','birthdate','ssn']
     i = 0
 
     while True: 
-        data = faker.profile(fields)
-        data['timestamp'] = datetime.now()
-        df = pd.DataFrame(data, index = [i])
+        data = faker.profile(fields) # using faker to generat e basic profile information based on fields list
+        data['timestamp'] = datetime.now() # creating timestamp that is the current local time
+        df = pd.DataFrame(data, index = [i]) # creating dataframe from generate profile information in data object
         print(f"Inserting data {data}")
-        df.to_sql('USERS',conn, if_exists='append')
-        i +=1 
-        sleep(1)
+        df.to_sql('USERS',conn, if_exists='append') # creates new tables 'USERS' using conn engine
+        i +=1 # i adding 1 to i variable and assigning result to that variable
+        sleep(1) # every one second
